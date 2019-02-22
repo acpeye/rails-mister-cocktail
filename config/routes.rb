d3e59ae5110c00a9623264713222717a7c83e8c7
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-02-22 16:02:03 +0900

    deleted auto generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to "coctails#index"
+  root to: "coctails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
